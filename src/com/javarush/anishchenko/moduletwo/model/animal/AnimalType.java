7b763751ddc323b460875eb4e82df7a630d12f81
Alena Anishchenko <alena.anishchenko@gmail.com>
2022-08-25 00:17:47 +0300

    settlement of the island by animals

---
+++ src/com/javarush/anishchenko/moduletwo/model/animal/AnimalType.java
@@ -0,0 +1,25 @@
+package com.javarush.anishchenko.moduletwo.model.animal;
+
+public enum AnimalType {
+    BEAR,
+    BOA,
+    BOAR,
+    BUFFALO,
+    DEER,
+    DUCK,
+    EAGLE,
+    FOX,
+    GOAT,
+    HORSE,
+    ISLANDS,
+    MOUSE,
+    RABBIT,
+    SHEEP,
+    WOLF ,
+    CATERPILLAR;
+
+    public static AnimalType[] ANIMALS = new AnimalType[] {
+            BEAR, BOA, BOAR, BUFFALO, DEER, DUCK, EAGLE, FOX, GOAT, HORSE, ISLANDS,
+            MOUSE, RABBIT, SHEEP, WOLF, CATERPILLAR
+    };
+}
